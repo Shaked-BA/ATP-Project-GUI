fe8f9c10faeec4a32afb84ff054f05d99fa4c527
Shaked <benahash@post.bgu.ac.il>
2022-06-18 19:09:29 +0300

    Initialized project's classes.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,11 @@
 module com.example.atpprojectgui {
     requires javafx.controls;
     requires javafx.fxml;
+    requires ATPProjectJAR;
 
 
     opens com.example.atpprojectgui to javafx.fxml;
     exports com.example.atpprojectgui;
+    exports View;
+    opens View to javafx.fxml;
 }
